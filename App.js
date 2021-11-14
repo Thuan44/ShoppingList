843500d8cb95a171477c95bb1b67d8ad4cf30a84
Cong-Thuan Do <congthuan274@gmail.com>
2021-11-14 16:57:48 +0100

    App finished

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native'
+import { v4 as uuid } from 'uuid'
+import Header from './components/Header'
+import ListItem from './components/ListItem'
+import AddItem from './components/AddItem'
 
 const App = () => {
+  const [items, setItems] = useState([
+    { id: uuid(), text: 'Milk' },
+    { id: uuid(), text: 'Bread' },
+    { id: uuid(), text: 'Juice' },
+    { id: uuid(), text: 'Rice' },
+  ])
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id )
+    })
+
+  }
+
+  const addItem = (text) => {
+    if(!text){
+      Alert.alert('Error', 'Please enter an item')
+    }else{
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems]
+      })
+    }
+  }
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem}/>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem} />}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
 
 export default App
 
